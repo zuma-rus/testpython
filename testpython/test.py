8249707bce7b512ba05651b237b14419a126b1ee
MXM-PC <jawsik@gmail.com>
2017-11-03 00:59:37 +0300

    преобразовал структуру папок для того, чтобы работало виртуальное окружение

--- testpython/test.py
+++ testpython/test.py
@@ -6,8 +6,10 @@ print("version = ", sys.version)
 print("Здорова")
 print("Новая запись, для баласта, так сказать")
 
+print("Запись уже сделанная другим пользователем. По факту просто на другом компьютере")
+
 def test(test):
-    print("function test complite:" , str(test))
+    print("function test complite:", str(test))
 
 def deli():
     print("function deli complite.")
