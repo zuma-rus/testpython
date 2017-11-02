386ad55e9474b3f1d9590d3b58c155275465b6ad
PC-4PU <zumas@yandex.ru>
2017-11-02 18:02:49 +0300

    добавил две тестовые функции. test и deli

--- test.py
+++ test.py
@@ -5,3 +5,9 @@ print("version = ", sys.version)
 
 print("Здорова")
 print("Новая запись, для баласта, так сказать")
+
+def test(test):
+    print("function test complite:" , str(test))
+
+def deli():
+    print("function deli complite.")
